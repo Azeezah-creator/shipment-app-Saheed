7a02399729582eda0bd0ab13c6fd2bf4c6413ea3
Azeezah <azeezahadeyemi4@gmail.com>
2025-11-13 06:31:14 +0100

    Hopefully, final fixing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@testing-library)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 };
