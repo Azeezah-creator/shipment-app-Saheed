0074692f06f6cf4b237795f78876770c5be42ac8
Azeezah <azeezahadeyemi4@gmail.com>
2025-11-13 06:37:48 +0100

    Go

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@testing-library)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@testing-library|@react-native-community)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 };
+
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,12 @@
-import '@testing-library/jest-native/extend-expect';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
+// Mock AsyncStorage
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+// Mock NetInfo
+jest.mock('@react-native-community/netinfo', () => ({
+  addEventListener: jest.fn(),
+  fetch: jest.fn(() => Promise.resolve({ isConnected: true })),
+}));
+
+// (Optional) mock any other native modules that might cause errors later
